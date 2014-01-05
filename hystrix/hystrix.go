6b0f6d4c7c3a3b5793671b26419b8bf0ff91e15f
Keith Thornhill <keith.thornhill@gmail.com>
2014-01-05 01:18:27 -0800

    package comment to test godoc

--- hystrix/hystrix.go
+++ hystrix/hystrix.go
@@ -1,3 +1,6 @@
+// Hystrix is a latency and fault tolerance library designed to isolate points of access to remote systems, services and 3rd party libraries, stop cascading failure and enable resilience in complex distributed systems where failure is inevitable.
+//
+// Based on the java project of the same name, by Netflix. https://github.com/Netflix/Hystrix
 package hystrix
 
 type RunFunc func(chan Result)
