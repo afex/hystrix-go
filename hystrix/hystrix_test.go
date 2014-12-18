e6d5c1f414bf3023f744fba867d4ea9b2cf2c9d1
Tarrant <tarrant@keyneston.com>
2014-12-17 20:02:50 -0800

    Add test for timeout of fallback function

--- hystrix/hystrix_test.go
+++ hystrix/hystrix_test.go
@@ -86,6 +86,27 @@ func TestTimeoutEmptyFallback(t *testing.T) {
 	}
 }
 
+func TestTimeoutFallback(t *testing.T) {
+	SetTimeout("timeout", time.Millisecond*100)
+
+	resultChan := make(chan int)
+	errChan := Go("timeout", func() error {
+		time.Sleep(1 * time.Second)
+		resultChan <- 1
+		return nil
+	}, func(err error) error {
+		time.Sleep(1 * time.Second)
+		resultChan <- 1
+		return nil
+	})
+
+	select {
+	case _ = <-resultChan:
+		t.Errorf("Should not get an response from resultChan")
+	case _ = <-errChan:
+	}
+}
+
 // TODO: how can we be sure the fallback is triggered from full pool.  error type?
 func TestMaxConcurrent(t *testing.T) {
 	SetConcurrency("max_concurrent", 2)
