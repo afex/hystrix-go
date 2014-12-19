e0071fcc298ab8f519ee60f87b3c2fc6c58c6d11
Tarrant <tarrant@keyneston.com>
2014-12-19 13:59:07 -0800

    Revert "Format comments"
    This reverts commit 4aa8a5aeed6979bf666854febacbbc1427453e00.
    
    At Keith's request

--- hystrix/hystrix.go
+++ hystrix/hystrix.go
@@ -3,8 +3,7 @@
 // cascading failure and enable resilience in complex distributed systems where
 // failure is inevitable.
 //
-// Based on the java project of the same name, by Netflix.
-// https://github.com/Netflix/Hystrix
+// Based on the java project of the same name, by Netflix. https://github.com/Netflix/Hystrix
 package hystrix
 
 import (
@@ -16,21 +15,20 @@ import (
 type runFunc func() error
 type fallbackFunc func(error) error
 
-// Go runs your function while tracking the health of previous calls to it. If
-// your function begins slowing down or failing repeatedly, we will block new
-// calls to it for you to give the dependent service time to repair.
+// Go runs your function while tracking the health of previous calls to it.
+// If your function begins slowing down or failing repeatedly, we will block
+// new calls to it for you to give the dependent service time to repair.
 //
-// Define a fallback function if you want to define some code to execute during
-// outages.
+// Define a fallback function if you want to define some code to execute during outages.
 func Go(name string, run runFunc, fallback fallbackFunc) chan error {
 	start := time.Now()
 
 	errChan := make(chan error, 1)
 	finished := make(chan bool, 1)
 
-	// dont have methods with explicit params and returns let data come in and
-	// out naturally, like with any closure explicit error return to give place
-	// for us to kill switch the operation (fallback)
+	// dont have methods with explicit params and returns
+	// let data come in and out naturally, like with any closure
+	// explicit error return to give place for us to kill switch the operation (fallback)
 
 	circuit, err := GetCircuit(name)
 	if err != nil {
@@ -47,10 +45,9 @@ func Go(name string, run runFunc, fallback fallbackFunc) chan error {
 			return
 		}
 
-		// Circuits get opened when recent executions have shown to have a high
-		// error rate. Rejecting new executions allows backends to recover, and
-		// the circuit will allow new traffic when it feels a healthly state
-		// has returned.
+		// Circuits get opened when recent executions have shown to have a high error rate.
+		// Rejecting new executions allows backends to recover, and the circuit will allow
+		// new traffic when it feels a healthly state has returned.
 		if circuit.IsOpen() {
 			reportEvent(circuit, "short-circuit", start, 0)
 			err := tryFallback(circuit, start, 0, fallback, errors.New("circuit open"))
@@ -62,11 +59,9 @@ func Go(name string, run runFunc, fallback fallbackFunc) chan error {
 
 		// As backends falter, requests take longer but don't always fail.
 		//
-		// When requests slow down but the incoming rate of requests stays the
-		// same, you have to run more at a time to keep up. By controlling
-		// concurrency during these situations, you can shed load which
-		// accumulates due to the increasing ratio of active commands to
-		// incoming requests.
+		// When requests slow down but the incoming rate of requests stays the same, you have to
+		// run more at a time to keep up. By controlling concurrency during these situations, you can
+		// shed load which accumulates due to the increasing ratio of active commands to incoming requests.
 		select {
 		case ticket := <-tickets:
 			defer func() { tickets <- ticket }()
