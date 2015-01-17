042afefeb337bc48c076063a9a02190347a29a62
Keith Thornhill <kthornhill@twitch.tv>
2015-01-17 00:04:32 -0800

    refactor tests

--- hystrix/eventstream_test.go
+++ hystrix/eventstream_test.go
@@ -9,16 +9,10 @@ import (
 	"time"
 )
 
-func TestEventStream(t *testing.T) {
-	hystrixStreamHandler := NewStreamHandler()
-	hystrixStreamHandler.Start()
-	defer hystrixStreamHandler.Stop()
-	server := httptest.NewServer(hystrixStreamHandler)
-	defer server.Close()
-
+func sleepingCommand(t *testing.T, name string, duration time.Duration) {
 	done := make(chan bool)
-	errChan := Go("eventstream", func() error {
-		time.Sleep(100 * time.Millisecond)
+	errChan := Go(name, func() error {
+		time.Sleep(duration)
 		done <- true
 		return nil
 	}, nil)
@@ -29,13 +23,45 @@ func TestEventStream(t *testing.T) {
 	case err := <-errChan:
 		t.Fatal(err)
 	}
+}
 
-	res, err := http.Get(server.URL)
+type EventStreamTestServer struct {
+	*httptest.Server
+	EventStreamer
+}
+
+type EventStreamer interface {
+	Stop()
+}
+
+func (s *EventStreamTestServer) stopTestServer() error {
+	s.Close()
+	s.Stop()
+
+	return nil
+}
+
+func startTestServer() *EventStreamTestServer {
+	hystrixStreamHandler := NewStreamHandler()
+	hystrixStreamHandler.Start()
+	return &EventStreamTestServer{
+		httptest.NewServer(hystrixStreamHandler),
+		hystrixStreamHandler,
+	}
+}
+
+// grabFirstFromStream reads on the http request until we see the first
+// full result printed
+func grabFirstFromStream(t *testing.T, url string) streamCmdEvent {
+	res, err := http.Get(url)
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer res.Body.Close()
 
+	// since the event stream connection doesn't ever close,
+	// we only read until we have the message we want and
+	// disconnect.
 	buf := []byte{0}
 	data := ""
 	for {
@@ -54,6 +80,16 @@ func TestEventStream(t *testing.T) {
 
 	var event streamCmdEvent
 	json.Unmarshal([]byte(data), &event)
+
+	return event
+}
+
+func TestEventStream(t *testing.T) {
+	server := startTestServer()
+	defer server.stopTestServer()
+
+	sleepingCommand(t, "eventstream", 100*time.Millisecond)
+	event := grabFirstFromStream(t, server.URL)
 
 	if event.Name != "eventstream" {
 		t.Fatal("metrics did not match command name")
