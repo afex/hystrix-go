cd84cf4b98a14d56e025a0e931db9ace2b44a16d
Keith Thornhill <kthornhill@twitch.tv>
2015-01-27 22:32:28 -0800

    copy stuff from readme into godoc

--- hystrix/hystrix.go
+++ hystrix/hystrix.go
@@ -1,9 +1,3 @@
-// Package hystrix is a latency and fault tolerance library designed to isolate
-// points of access to remote systems, services and 3rd party libraries, stop
-// cascading failure and enable resilience in complex distributed systems where
-// failure is inevitable.
-//
-// Based on the java project of the same name, by Netflix. https://github.com/Netflix/Hystrix
 package hystrix
 
 import (
