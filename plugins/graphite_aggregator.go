1f77ef6a5be60a42757a1bd8a80f91e16b62afca
tietang <wtt.2012@gmail.com>
2016-09-13 11:10:42 +0800

    deps

--- plugins/graphite_aggregator.go
+++ plugins/graphite_aggregator.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/afex/hystrix-go/hystrix/metric_collector"
+	"github.com/tietang/hystrix-go/hystrix/metric_collector"
 	"github.com/rcrowley/go-metrics"
 )
 
--- plugins/statsd_collector.go
+++ plugins/statsd_collector.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/afex/hystrix-go/hystrix/metric_collector"
+	"github.com/tietang/hystrix-go/hystrix/metric_collector"
 	"github.com/cactus/go-statsd-client/statsd"
 )
 
