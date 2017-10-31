bac535821faa59a40663b8541f33619b232f690a
邝昌浪 <kuangchanglang@bytedance.com>
2017-10-31 12:31:26 +0800

    make rolling window configurable
    Change-Id: I9cf603df0717a8f174cf71ea6c236967d21d356c

--- hystrix/rolling/rolling_test.go
+++ hystrix/rolling/rolling_test.go
@@ -36,6 +36,20 @@ func TestAvg(t *testing.T) {
 	})
 }
 
+func TestWindowAvg(t *testing.T) {
+	Convey("when adding values to a rolling number", t, func() {
+		n, _ := NewNumberWithWindow(2)
+		for _, x := range []float64{0.5, 1.5, 2.5, 3.5, 4.5} {
+			n.Increment(x)
+			time.Sleep(1 * time.Second)
+		}
+
+		Convey("it should calculate the average over the number of configured buckets", func() {
+			So(n.Avg(time.Now()), ShouldEqual, 4) // (3.5+4.5)/2
+		})
+	})
+}
+
 func BenchmarkRollingNumberIncrement(b *testing.B) {
 	n := NewNumber()
 
