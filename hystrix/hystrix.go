918c0781f0d15bb2059ee60c2fc027f61893898a
Keith Thornhill <kthornhill@twitch.tv>
2018-01-23 00:28:38 -0800

    refactor Go/Do to manipulate underlying command struct

--- hystrix/hystrix.go
+++ hystrix/hystrix.go
@@ -25,15 +25,16 @@ func (e CircuitError) Error() string {
 type command struct {
 	sync.Mutex
 
-	ticket       *struct{}
-	start        time.Time
-	errChan      chan error
-	finished     chan bool
-	circuit      *CircuitBreaker
-	run          runFunc
-	fallback     fallbackFunc
-	runDuration  time.Duration
-	events       []string
+	name        string
+	ticket      *struct{}
+	start       time.Time
+	errChan     chan error
+	finished    chan bool
+	circuit     *CircuitBreaker
+	run         runFunc
+	fallback    fallbackFunc
+	runDuration time.Duration
+	events      []string
 }
 
 var (
@@ -52,127 +53,21 @@ var (
 // Define a fallback function if you want to define some code to execute during outages.
 func Go(name string, run runFunc, fallback fallbackFunc) chan error {
 	cmd := &command{
-		run:          run,
-		fallback:     fallback,
-		start:        time.Now(),
-		errChan:      make(chan error, 1),
-		finished:     make(chan bool, 1),
+		name:     name,
+		run:      run,
+		fallback: fallback,
+		start:    time.Now(),
+		errChan:  make(chan error, 1),
+		finished: make(chan bool, 1),
 	}
 
-	// dont have methods with explicit params and returns
-	// let data come in and out naturally, like with any closure
-	// explicit error return to give place for us to kill switch the operation (fallback)
-
-	circuit, _, err := GetCircuit(name)
-	if err != nil {
-		cmd.errChan <- err
-		return cmd.errChan
-	}
-	cmd.circuit = circuit
-	ticketCond := sync.NewCond(cmd)
-	ticketChecked := false
-	// When the caller extracts error from returned errChan, it's assumed that
-	// the ticket's been returned to executorPool. Therefore, returnTicket() can
-	// not run after cmd.errorWithFallback().
-	returnTicket := func() {
-		cmd.Lock()
-		// Avoid releasing before a ticket is acquired.
-		for !ticketChecked {
-			ticketCond.Wait()
-		}
-		cmd.circuit.executorPool.Return(cmd.ticket)
-		cmd.Unlock()
-	}
-	// Shared by the following two goroutines. It ensures only the faster
-	// goroutine runs errWithFallback() and reportAllEvent().
-	returnOnce := &sync.Once{}
-	reportAllEvent := func() {
-		err := cmd.circuit.ReportEvent(cmd.events, cmd.start, cmd.runDuration)
-		if err != nil {
-			log.Print(err)
-		}
-	}
-
-	go func() {
-		defer func() { cmd.finished <- true }()
-
-		// Circuits get opened when recent executions have shown to have a high error rate.
-		// Rejecting new executions allows backends to recover, and the circuit will allow
-		// new traffic when it feels a healthly state has returned.
-		if !cmd.circuit.AllowRequest() {
-			cmd.Lock()
-			// It's safe for another goroutine to go ahead releasing a nil ticket.
-			ticketChecked = true
-			ticketCond.Signal()
-			cmd.Unlock()
-			returnOnce.Do(func() {
-				returnTicket()
-				cmd.errorWithFallback(ErrCircuitOpen)
-				reportAllEvent()
-			})
-			return
-		}
-
-		// As backends falter, requests take longer but don't always fail.
-		//
-		// When requests slow down but the incoming rate of requests stays the same, you have to
-		// run more at a time to keep up. By controlling concurrency during these situations, you can
-		// shed load which accumulates due to the increasing ratio of active commands to incoming requests.
-		cmd.Lock()
-		select {
-		case cmd.ticket = <-circuit.executorPool.Tickets:
-			ticketChecked = true
-			ticketCond.Signal()
-			cmd.Unlock()
-		default:
-			ticketChecked = true
-			ticketCond.Signal()
-			cmd.Unlock()
-			returnOnce.Do(func() {
-				returnTicket()
-				cmd.errorWithFallback(ErrMaxConcurrency)
-				reportAllEvent()
-			})
-			return
-		}
-
-		runStart := time.Now()
-		runErr := run()
-		returnOnce.Do(func() {
-			defer reportAllEvent()
-			cmd.runDuration = time.Since(runStart)
-			returnTicket()
-			if runErr != nil {
-				cmd.errorWithFallback(runErr)
-				return
-			}
-			cmd.reportEvent("success")
-		})
-	}()
-
-	go func() {
-		timer := time.NewTimer(getSettings(name).Timeout)
-		defer timer.Stop()
-
-		select {
-		case <-cmd.finished:
-			// returnOnce has been executed in another goroutine
-		case <-timer.C:
-			returnOnce.Do(func() {
-				returnTicket()
-				cmd.errorWithFallback(ErrTimeout)
-				reportAllEvent()
-			})
-			return
-		}
-	}()
-
-	return cmd.errChan
+	return cmd.Go()
 }
 
 // Do runs your function in a synchronous manner, blocking until either your function succeeds
 // or an error is returned, including hystrix circuit errors
 func Do(name string, run runFunc, fallback fallbackFunc) error {
+
 	done := make(chan struct{}, 1)
 
 	r := func() error {
@@ -195,12 +90,18 @@ func Do(name string, run runFunc, fallback fallbackFunc) error {
 		return nil
 	}
 
-	var errChan chan error
-	if fallback == nil {
-		errChan = Go(name, r, nil)
-	} else {
-		errChan = Go(name, r, f)
+	cmd := &command{
+		name:     name,
+		run:      r,
+		start:    time.Now(),
+		errChan:  make(chan error, 1),
+		finished: make(chan bool, 1),
 	}
+	if fallback != nil {
+		cmd.fallback = f
+	}
+
+	errChan := cmd.Go()
 
 	select {
 	case <-done:
@@ -208,6 +109,118 @@ func Do(name string, run runFunc, fallback fallbackFunc) error {
 	case err := <-errChan:
 		return err
 	}
+}
+
+func (c *command) Go() chan error {
+	// dont have methods with explicit params and returns
+	// let data come in and out naturally, like with any closure
+	// explicit error return to give place for us to kill switch the operation (fallback)
+
+	circuit, _, err := GetCircuit(c.name)
+	if err != nil {
+		c.errChan <- err
+		return c.errChan
+	}
+	c.circuit = circuit
+	ticketCond := sync.NewCond(c)
+	ticketChecked := false
+	// When the caller extracts error from returned errChan, it's assumed that
+	// the ticket's been returned to executorPool. Therefore, returnTicket() can
+	// not run after cmd.errorWithFallback().
+	returnTicket := func() {
+		c.Lock()
+		// Avoid releasing before a ticket is acquired.
+		for !ticketChecked {
+			ticketCond.Wait()
+		}
+		c.circuit.executorPool.Return(c.ticket)
+		c.Unlock()
+	}
+	// Shared by the following two goroutines. It ensures only the faster
+	// goroutine runs errWithFallback() and reportAllEvent().
+	returnOnce := &sync.Once{}
+	reportAllEvent := func() {
+		err := c.circuit.ReportEvent(c.events, c.start, c.runDuration)
+		if err != nil {
+			log.Print(err)
+		}
+	}
+
+	go func() {
+		defer func() { c.finished <- true }()
+
+		// Circuits get opened when recent executions have shown to have a high error rate.
+		// Rejecting new executions allows backends to recover, and the circuit will allow
+		// new traffic when it feels a healthly state has returned.
+		if !c.circuit.AllowRequest() {
+			c.Lock()
+			// It's safe for another goroutine to go ahead releasing a nil ticket.
+			ticketChecked = true
+			ticketCond.Signal()
+			c.Unlock()
+			returnOnce.Do(func() {
+				returnTicket()
+				c.errorWithFallback(ErrCircuitOpen)
+				reportAllEvent()
+			})
+			return
+		}
+
+		// As backends falter, requests take longer but don't always fail.
+		//
+		// When requests slow down but the incoming rate of requests stays the same, you have to
+		// run more at a time to keep up. By controlling concurrency during these situations, you can
+		// shed load which accumulates due to the increasing ratio of active commands to incoming requests.
+		c.Lock()
+		select {
+		case c.ticket = <-circuit.executorPool.Tickets:
+			ticketChecked = true
+			ticketCond.Signal()
+			c.Unlock()
+		default:
+			ticketChecked = true
+			ticketCond.Signal()
+			c.Unlock()
+			returnOnce.Do(func() {
+				returnTicket()
+				c.errorWithFallback(ErrMaxConcurrency)
+				reportAllEvent()
+			})
+			return
+		}
+
+		runStart := time.Now()
+		runErr := c.run()
+		returnOnce.Do(func() {
+			defer reportAllEvent()
+			c.runDuration = time.Since(runStart)
+			returnTicket()
+			if runErr != nil {
+				c.errorWithFallback(runErr)
+				return
+			}
+			c.reportEvent("success")
+		})
+	}()
+
+	go func() {
+		timer := time.NewTimer(getSettings(c.name).Timeout)
+		defer timer.Stop()
+
+		select {
+		case <-c.finished:
+			// returnOnce has been executed in another goroutine
+		case <-timer.C:
+			returnOnce.Do(func() {
+				returnTicket()
+				c.errorWithFallback(ErrTimeout)
+				reportAllEvent()
+			})
+			return
+		}
+	}()
+
+	return c.errChan
 }
 
 func (c *command) reportEvent(eventType string) {
