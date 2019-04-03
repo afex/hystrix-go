4f7f0a216ae56fb0ef5f800521762de684b5598f
Lokesh Jain <lokesh.jain@continuum.net>
2019-04-03 16:20:03 +0530

    Updated Import statement

--- hystrix/circuit.go
+++ hystrix/circuit.go
@@ -6,7 +6,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/ContinuumLLC/hystrix-go/hystrix/callback"
+	"github.com/afex/hystrix-go/hystrix/callback"
 )
 
 // CircuitBreaker is created for each ExecutorPool to track whether requests
