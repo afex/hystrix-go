a8b9bc0bfa7d8b28c25ac790fd9a71a8846b525b
Frederic G. MARAND <frederic.marand@deliveroo.co.uk>
2021-09-13 16:48:22 +0200

    Issue #119: convert to Go 1.17.

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/afex/hystrix-go
+
+go 1.17
+
+require (
+	github.com/DataDog/datadog-go v4.8.2+incompatible
+	github.com/cactus/go-statsd-client v2.0.2+incompatible
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475
+	github.com/smartystreets/goconvey v1.6.4
+)
+
+require (
+	github.com/Microsoft/go-winio v0.5.0 // indirect
+	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210124154548-22da62e12c0c // indirect
+)
--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -5,19 +5,20 @@ wget -q https://storage.googleapis.com/golang/go1.17.1.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
 
 apt-get update
-apt-get -y install git mercurial apache2-utils
+apt-get -y install git mercurial apache2-utils gcc # GCC needed for CGO dependencies
+apt-get upgrade -y
 
 echo 'export PATH=$PATH:/usr/local/go/bin:/go/bin
 export GOPATH=/go' >> /home/vagrant/.profile
 
 source /home/vagrant/.profile
 
-go get golang.org/x/tools/cmd/goimports
-go get github.com/golang/lint/golint
+go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/lint/golint@latest
 go get github.com/smartystreets/goconvey/convey
-go get github.com/cactus/go-statsd-client/statsd
-go get github.com/rcrowley/go-metrics
-go get github.com/DataDog/datadog-go/statsd
+go get github.com/cactus/go-statsd-client/statsd@v2
+go get github.com/rcrowley/go-metrics@latest
+go get github.com/DataDog/datadog-go/statsd@latest
 
 chown -R vagrant:vagrant /go
 
