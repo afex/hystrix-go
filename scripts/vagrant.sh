8c2cceddb84f53aee22d17010c5fcf161c687a68
Frédéric G. MARAND <fgm@osinet.fr>
2021-09-13 16:07:31 +0200

    Update vagrant.sh
    Upgrade to Go 1.17.1

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-wget -q https://storage.googleapis.com/golang/go1.9.4.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.9.4.linux-amd64.tar.gz
+wget -q https://storage.googleapis.com/golang/go1.17.1.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
 
 apt-get update
 apt-get -y install git mercurial apache2-utils
